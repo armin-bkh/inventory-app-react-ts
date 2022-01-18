e4ac424f5be194fbe4cd05031e714b135c1261d0
armin bakhshi <arminbkh0921@yahoo.com>
2022-01-18 14:20:11 +0330

    dynamic return for edit and add actions

--- src/Components/Filters/FilterForm/FilterForm.tsx
+++ src/Components/Filters/FilterForm/FilterForm.tsx
@@ -1,73 +1,91 @@
-import { useFormik, FormikProps } from "formik"
+import { useFormik, FormikProps } from "formik";
 import { useEffect, useState } from "react";
 import { useToasts } from "react-toast-notifications";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import Input from "../../Common/Input/Input";
-import { useInventory, useInventoryActions } from "../../Provider/InventoryProvider";
+import {
+  useInventory,
+  useInventoryActions,
+} from "../../Provider/InventoryProvider";
 import { filters } from "../../Provider/InventoryProvider.type";
 import { isExistFilter } from "../../Utils/isExistFilter";
 
 export interface formValueType {
-    filter: string
+  filter: string;
 }
 
 const initialValues = {
-    filter: ''
-}
+  filter: "",
+};
 
 const validationSchema = Yup.object({
-    filter: Yup.string().required('filter name is required.')
-})
+  filter: Yup.string().required("filter name is required."),
+});
 
 interface filterFormProps {
-    id: number | null;
-    handleEdit: (filter: string) => void;
+  id: number | null;
+  handleEdit: (filter: string) => void;
 }
 
 const FilterForm = ({ id, handleEdit }: filterFormProps) => {
-    const [formValues, setFormValues] = useState<formValueType | null>(null);
-    const { filters } = useInventory();
-    const { addFilterHandler } = useInventoryActions();
-    const { addToast } = useToasts();
+  const [formValues, setFormValues] = useState<formValueType | null>(null);
+  const { filters } = useInventory();
+  const { addFilterHandler } = useInventoryActions();
+  const { addToast } = useToasts();
 
-    useEffect(()=> {
-       if(id){
-           setFormValues({
-               filter: filters.find(filter => filter.id === id)?.value || '',
-           });
-       }
-    }, [id])
-
-    const onSubmit = (values: formValueType) => {
-        if(id) {
-            handleEdit(values.filter);
-            setFormValues(null);
-            addToast(`${values.filter} successfuly edited`, {appearance: 'success'});
-            formik.handleReset();
-            return;
-        }
-        if(!isExistFilter(filters, values.filter)) {
-            addFilterHandler(values);
-            addToast(`${values.filter} successfuly added`, {appearance: 'success'});
-            formik.handleReset();
-        }
-        else addToast(`${values.filter} is already exist`, {appearance: 'error'});
+  useEffect(() => {
+    if (id) {
+      setFormValues({
+        filter: filters.find((filter) => filter.id === id)?.value || "",
+      });
     }
+  }, [id]);
 
-    const formik: FormikProps<formValueType> = useFormik<formValueType>({ 
-        initialValues: formValues ||  initialValues,
-        validationSchema,
-        onSubmit,
-        validateOnMount: true,
-        enableReinitialize: true,
-     });
+  const onSubmit = (values: formValueType) => {
+    if (id) {
+      handleEdit(values.filter);
+      setFormValues(null);
+      addToast(`${values.filter} successfuly edited`, {
+        appearance: "success",
+      });
+      formik.handleReset();
+      return;
+    }
+    if (!isExistFilter(filters, values.filter)) {
+      addFilterHandler(values);
+      addToast(`${values.filter} successfuly added`, { appearance: "success" });
+      formik.handleReset();
+    } else
+      addToast(`${values.filter} is already exist`, { appearance: "error" });
+  };
 
-    return (
-        <form className={`rounded-lg shadow-lg p-5`} onSubmit={formik.handleSubmit}>
-            <Input type="text" name="filter" id="filterInput" lbl="filter" formik={formik} />
-            <button className="rounded-md bg-emerald-600 text-white px-3 py-1 mt-5 disabled:bg-opacity-50" type="submit" disabled={!formik.isValid}>Add Filter</button>
-        </form>
-    )
-}
+  const formik: FormikProps<formValueType> = useFormik<formValueType>({
+    initialValues: formValues || initialValues,
+    validationSchema,
+    onSubmit,
+    validateOnMount: true,
+    enableReinitialize: true,
+  });
+
+  return (
+    <form className={`rounded-lg shadow-lg p-5`} onSubmit={formik.handleSubmit}>
+      <Input
+      autoFocus
+        type="text"
+        name="filter"
+        id="filterInput"
+        lbl="filter"
+        formik={formik}
+      />
+      <button
+        className="rounded-md bg-emerald-600 text-white px-3 py-1 mt-5 disabled:bg-opacity-50"
+        type="submit"
+        disabled={!formik.isValid}
+      >
+        {!id ? "Add Filter" : "Edit Filter"}
+      </button>
+    </form>
+  );
+};
 
 export default FilterForm;
