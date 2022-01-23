1d0213d42569f6ba16af8cea334f9ca303908393
armin-bkh <arminbkh0921@yahoo.com>
2022-01-23 15:01:27 +0330

    handle close option items when clicked outside the SelectBox component

--- src/Components/Common/SelectBox/SelectBox.tsx
+++ src/Components/Common/SelectBox/SelectBox.tsx
@@ -1,32 +1,61 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { string } from "yup";
 import { filters } from "../../Provider/InventoryProvider.type";
-import styles from './SelectBox.module.scss';
+import styles from "./SelectBox.module.scss";
 
 interface selectBoxProps {
-    width?: number | string,
-    value: string,
-    onChange: (selectedOption: filters) => void,
-    options: filters[];
+  width?: number | string;
+  value: string;
+  onChange: (selectedOption: filters) => void;
+  options: filters[];
 }
 
 const SelectBox = ({ value, onChange, options, width }: selectBoxProps) => {
-    const [inputValue, setInputValue] = useState<string>(value || '');
+  const [inputValue, setInputValue] = useState<string>(value || "");
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const optionsRef = useRef<HTMLDivElement>(null!);
 
-    const changeOptionHandler = (selectedOption: filters) => {
-        setInputValue(selectedOption.label);
-        onChange(selectedOption);
-    }
+    useEffect(()=> {
+        function handleCloseOptions(e: any){
+            if(optionsRef.current && !optionsRef.current.contains(e.target)){
+                setIsShow(false)
+            }
+        }
 
-    return (
-        <div className={styles.selectBox}>
-            <div>
-                <input style={{ width }} className={styles.selectBoxInput} type="text" value={inputValue} onChange={(e)=> setInputValue(e.target.value)}/>
+        document.addEventListener("mousedown", handleCloseOptions);
+        return () => {
+            document.removeEventListener("mousedown", handleCloseOptions);
+        };
+    }, [optionsRef])
+
+  const changeOptionHandler = (selectedOption: filters) => {
+    setInputValue(selectedOption.label);
+    onChange(selectedOption);
+    setIsShow(false);
+  };
+
+  return (
+    <div ref={optionsRef} className={styles.selectBox}>
+      <div>
+        <input
+            onClick={()=> setIsShow(true)}
+          style={{ width }}
+          className={styles.selectBoxInput}
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </div>
+      {isShow && (
+        <div>
+          {options.map((option) => (
+            <div onClick={() => changeOptionHandler(option)} key={option.id}>
+              {option.label}
             </div>
-            <div>
-                {options.map(option => <div onClick={()=> changeOptionHandler(option)} key={option.id}>{option.label}</div>)}
-            </div>
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 export default SelectBox;
